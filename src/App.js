99e5d518d7723272286e7cc99ab9a3e16d250fda
Tinamork <s376609@oslomet.no>
2024-11-25 23:02:22 +0100

    super enkel start på Profile, samtidig en enkel setup på Home og SidebarMenu.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import HomePage from "./Home/Index";
+import ProfilePage from "./Profile/Profile";
+import SidebarMenu from "./shared/SidebarMenu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="d-flex">
+                {/* Sidebar Menu */}
+                <SidebarMenu />
+
+                {/* Main Content */}
+                <Container className="flex-grow-1 p-4">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/Profile" element={<ProfilePage />} />
+                    </Routes>
+                </Container>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
